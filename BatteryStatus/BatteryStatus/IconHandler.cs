0b53db58ae0703a5a608514a40d8a0e009bf9101
Ramon Bollen <se_jake@hotmail.com>
2019-10-27 20:58:26 +0100

    Refactor Icon handling.

---
+++ BatteryStatus/BatteryStatus/IconHandler.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------
+//      Autor: Ramon Bollen
+//       File: BatteryPercentage.IconHandler.cs
+// Created on: 20191027
+//-----------------------------------------------
+
+using System;
+using System.Drawing;
+
+namespace BatteryPercentage
+{
+    /// <summary>
+    /// Create icons
+    /// </summary>
+    internal static class IconHandler
+    {
+        public static Icon Create(float percentage)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            using var iconBitmap = new Bitmap(256, 256);
+
+            var graphic = Graphics.FromImage(iconBitmap);
+            graphic.Clear(Color.Transparent);
+            graphic.DrawArc(
+                pen: new Pen(Color.White, 48),
+                rect: new Rectangle(32, 40, 192, 192),
+                startAngle: 270.0F,
+                sweepAngle: (360.0F / 100.0F) * percentage);
+
+            return Icon.FromHandle(iconBitmap.GetHicon());
+        }
+    }
+}
--- BatteryStatus/BatteryStatus/MainTray.cs
+++ BatteryStatus/BatteryStatus/MainTray.cs
@@ -4,10 +4,11 @@
 // Created on: 20191027
 //-----------------------------------------------
 
+using BatteryPercentage;
+
 using Hardcodet.Wpf.TaskbarNotification;
 
 using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace BatteryStatus
@@ -22,38 +23,22 @@ namespace BatteryStatus
         /// </summary>
         public MainTray()
         {
-            UpdateStatus(_taskBarIcon, SystemInformation.PowerStatus);
+            UpdateStatus(_taskBarIcon, SystemInformation.PowerStatus.BatteryLifePercent * 100);
 
             _getBatteryInformationTimer.Tick += BatteryInformationTimer_Tick;
-            _getBatteryInformationTimer.Interval = (int)new TimeSpan(0, 0, 1).TotalMilliseconds;
+            _getBatteryInformationTimer.Interval = (int)new TimeSpan(0, 0, 0, 0, 200).TotalMilliseconds;
             _getBatteryInformationTimer.Start();
         }
 
         private void BatteryInformationTimer_Tick(object sender, EventArgs e)
         {
-            UpdateStatus(_taskBarIcon, SystemInformation.PowerStatus);
+            UpdateStatus(_taskBarIcon, SystemInformation.PowerStatus.BatteryLifePercent * 100);
         }
 
-        private static void UpdateStatus(TaskbarIcon icon, PowerStatus status)
+        private static void UpdateStatus(TaskbarIcon icon, float perc)
         {
-            float battPerc = status.BatteryLifePercent * 100;
-
-            icon.Icon = CreateIcon(battPerc);
-            icon.ToolTipText = $"{battPerc.ToString()}%";
-        }
-
-        private static Icon CreateIcon(float percentage)
-        {
-            var bitmap = new Bitmap(256, 256);
-            var graphic = Graphics.FromImage(bitmap);
-            graphic.Clear(Color.Transparent);
-            graphic.DrawArc(
-                new Pen(Color.White, 48),
-                new Rectangle(32, 40, 192, 192),
-                270.0F,
-                (360.0F / 100.0F) * percentage);
-
-            return Icon.FromHandle(bitmap.GetHicon());
+            icon.Icon = IconHandler.Create(perc);
+            icon.ToolTipText = $"{perc.ToString()}%";
         }
 
         public void Dispose()
