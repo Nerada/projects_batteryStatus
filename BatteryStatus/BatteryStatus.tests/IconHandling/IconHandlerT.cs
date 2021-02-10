ca0600b4742ccaa878c7a013abd13605bcf34499
Nerada <5610242+Nerada@users.noreply.github.com>
2021-02-10 22:19:58 +0100

    Dummy test to generate icon image

---
+++ BatteryStatus/BatteryStatus.tests/IconHandling/IconHandlerT.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------
+//     Author: Ramon Bollen
+//      File: BatteryStatus.tests.IconHandlerT.cs
+// Created on: 20201207
+// -----------------------------------------------
+
+using System.Drawing.Imaging;
+
+using BatteryStatus.IconHandling;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BatteryStatus.tests.IconHandling
+{
+    [TestClass]
+    public class IconHandlerT
+    {
+        private readonly IconHandler _iconHandler = new IconHandler();
+
+        [TestMethod]
+        public void GenerateIcon()
+        {
+            //_iconHandler.IsCharging = true;
+            //_iconHandler.StayAwake = true;
+            //_iconHandler.Percentage = 66;
+
+            //var image = _iconHandler.Draw();
+
+            //image.Save("iconImage.png", ImageFormat.Png);
+        }
+    }
+}
